87e200332b12166c4cf03ccf51672805c6799842
Aravind <arviiartwork@gmail.com>
2025-04-09 07:15:29 +0000

    Trigger re-deployment

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "🎉 Hello from Aptible! Your first app is live!"
+    return "🎉 Hello from Aptible! Your first app is live now!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
